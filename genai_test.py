5cd3ada35db80e8b903534855a3a29bd507331f8
jibbydonut <jacobkilroy@outlook.com>
2025-04-14 23:24:48 +0100

    update .env and add gitignore

--- genai_test.py
+++ genai_test.py
@@ -1,9 +1,11 @@
 from google import genai
+from dotenv import load_dotenv
+import os
 
-client = genai.Client(api_key="AIzaSyBWjBKQFkUN4ou6xNJB_hdTnTl2Kpvhga8")
+client = genai.Client(api_key=os.getenv("GOOGLE_GENAI_API"))
 
 response = client.models.generate_content(
     model="gemini-2.0-flash", contents="Explain how AI works in a short paragraph"
 )
 
-print(response)
+print(response.text)
